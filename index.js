0a15969cd3580e9d239d1879c79289617c5561d0
sana-younis <sana.younis05@gmail.com>
2024-09-09 01:34:27 +0500

    Work Added

---
+++ index.js
@@ -0,0 +1,61 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user-route.js";
+import authRouter from "./routes/auth-route.js";
+import listingRouter from "./routes/listing-route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+
+dotenv.config();
+
+const corsOption = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// const __dirname = path.resolve();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(cors(corsOption));
+
+// const port = 3000;
+
+app.listen(3000, () => {
+  console.log(`Server is running on the port 3000`);
+});
+
+// api route
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
+app.get("/", (req, res) => {
+  app.use(express.static(path.resolve(__dirname, "client", "dist")));
+  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
